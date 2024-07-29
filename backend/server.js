d033d060029fe1e8eaceafc6c17c9cd8bad664d8
sandeep14032004 <sandeeppati69@gmail.com>
2024-07-29 09:10:08 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 dotenv.config();
+connectDB();
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API IS SUCCESSFULLY RUNNING");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singlechat = chats.find((c) => c._id === req.params.id);
-  res.send(singlechat);
-});
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`server started ${PORT}`));
+app.listen(PORT, console.log(`server started ${PORT}`.cyan.bold));
--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 
-function homepage() {
-  return <div>Home</div>;
-}
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
-export default homepage;
+const Homepage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        textAlign="center"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          GietConnect
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="blue">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default Homepage;
