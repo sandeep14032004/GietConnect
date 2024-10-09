1a0c63977a426b45c6b8fceec91ef2ef038d4d16
sandeep14032004 <sandeeppati69@gmail.com>
2024-10-09 22:46:33 +0530

    .

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -23,7 +23,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://giet-connect-zqds.onrender.com";
 var socket;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
